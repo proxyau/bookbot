aa00b33f9477addd69e770f9bdc01f3292272cc3
proxyau <stuart.lillywhite@gmail.com>
2024-10-21 15:15:18 +0800

    Added sorting capabilities

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     bookPath = "books/frankenstein.txt"
     text = get_book_text(bookPath)
     count = countWords(text)
-    print(count)
+    unique_dict = countUnique(text)
+    chars_sorted_list = chars_dict_to_sorted_list(unique_dict)
+    
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
 def get_book_text(bookPath):
     with open(bookPath) as f:
@@ -13,7 +19,23 @@ def countWords(text):
     return len(words)
 
 def countUnique(text):
-    words = text.split().lower()
-    print(words)
-    
+    uniqueChar = {}
+    for word in text:
+        character = word.lower() 
+        if character not in uniqueChar:
+            uniqueChar[character] = 1
+        else:
+            uniqueChar[character] += 1
+    return uniqueChar
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(unique_dict):
+    sorted_list = []
+    for character in unique_dict:
+        sorted_list.append({"char": character, "num": unique_dict[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 main()
